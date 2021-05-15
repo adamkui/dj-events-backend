b3f0fd420fa581193018f67eb55c6c30ae5fffaf
Kui Ádám <74501875+adamkui@users.noreply.github.com>
2021-05-15 18:21:38 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1b6f265ac39df988ceb301793c8144d8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b901223d-f620-4467-a59c-e6dc3a841246'
+};
